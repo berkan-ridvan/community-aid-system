746721eb5e54feb615a8929afe87f324b2141167
berkan-ridvan <berkanrd@gmail.com>
2025-05-02 20:12:38 -0500

    second commit v05.02.0812

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
